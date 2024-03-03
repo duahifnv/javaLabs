83af6dce52ee8dd045751cce48fd97bffdb9c297
duahifnv <max135603@gmail.com>
2024-03-03 22:08:54 +0300

    Added some files to lab3

--- lab3/src/Program.java
+++ lab3/src/Program.java
@@ -1,5 +1,6 @@
 public class Program {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("Лабораторная работа №3");
+        Lab3.Task1();
     }
 }
