81c4638d79e8d9adb67f1e451e97f1ba948f3d2e
duahifnv <max135603@gmail.com>
2024-03-03 22:30:38 +0300

    Завершена 1 задача 3 лабы

--- lab3/src/Lab3.java
+++ lab3/src/Lab3.java
@@ -1,12 +1,13 @@
 public class Lab3 {
     public static void t1() {
         System.out.println("Таблица значений функций");
-        System.out.printf("%10s|%15s|%15s\n", "x", "sin(x)", "e^x / x * lg(x)");
+        System.out.printf("%-10s| %-15s| %-15s\n", "x", "sin(x)", "e^x / x * lg(x)");
         double val1, val2;
+        String textArg;
         for (double arg = Math.PI/15; arg < Math.PI; arg += Math.PI/15) {
             val1 = Math.sin(arg);
             val2 = Math.exp(arg) / arg * Math.log(arg);
-            System.out.printf("%10.5f|%15.7f|%15.7f\n", arg, val1, val2);
+            System.out.printf("%-10.5f| %-15.7f| %-15.7f\n", arg, val1, val2);
         }
     }
 }
