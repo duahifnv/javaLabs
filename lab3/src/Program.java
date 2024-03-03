ff4b22125cdce105e740289f633522e0cac44b8c
duahifnv <max135603@gmail.com>
2024-03-03 22:20:43 +0300

    Добавил батник для запуска через консоль. Исправил вывод чисел

--- lab3/src/Program.java
+++ lab3/src/Program.java
@@ -1,6 +1,6 @@
 public class Program {
     public static void main(String[] args) throws Exception {
         System.out.println("Лабораторная работа №3");
-        Lab3.Task1();
+        Lab3.t1();
     }
 }
