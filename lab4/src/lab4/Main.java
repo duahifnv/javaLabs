c871c8f9c412dcbee3c0147361a89a4991e8754b
duahifnv <max135603@gmail.com>
2024-03-24 22:46:14 +0300

    ООП: Добавлен менеджер заданий (консольные аргументы)

--- lab4/src/lab4/Main.java
+++ lab4/src/lab4/Main.java
@@ -1,8 +1,38 @@
 package lab4;
 
 import lab4.taskList.taskList;
+import utils.TaskManager;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
-    public static void main(String[] args) {
-        taskList.task9();
+    public static void main(String... args) {
+        List<String> argList = new ArrayList<>(Arrays.asList(args));
+        switch (argList.size()) {
+            case 0 -> throw new Error("Укажите номер задачи");
+            case 1 ->
+            {
+                try {
+                    int taskIndex = Integer.parseInt(argList.get(0));
+                    TaskManager.Run(taskList.class, taskIndex);
+                }
+                catch(Exception NumberFormatException) {
+                    throw new Error("Указан нечисловой аргумент для номера задачи");
+                }
+            }
+            default ->
+            {
+                try {
+                    int taskIndex = Integer.parseInt(argList.get(0));
+                    argList.remove(0);
+                    TaskManager.Run(taskList.class, taskIndex, argList);
+                }
+                catch(Exception NumberFormatException) {
+                    throw new Error("Указан нечисловой аргумент для номера задачи");
+                }
+            }
+        }
     }
 }
