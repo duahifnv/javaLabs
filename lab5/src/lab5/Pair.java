205d2983a49e05b197064a52b2c2758f34d3b5e8
duahifnv <max135603@gmail.com>
2024-04-14 16:08:51 +0300

    Класс пар элементов

---
+++ lab5/src/lab5/Pair.java
@@ -0,0 +1,43 @@
+package lab5;
+
+/**
+ * <h2>Пара элементов</h2>
+ * @param <K>
+ * @param <V>
+ */
+public class Pair<K, V> {
+    public K first;
+    public V second;
+    private String label = "Pair";
+    // Конструктор без названия пары
+    public Pair(K first, V second) {
+        this.first = first;
+        this.second = second;
+    }
+    // Конструктор с названием пары
+    public Pair(K first, V second, String label) {
+        this.first = first;
+        this.second = second;
+        this.label = label;
+    }
+    // Статический метод создания новой пары
+    public static <K,V> Pair<K,V> make_pair(K first, V second) {
+        return new Pair<>(first, second);
+    }
+    // Сеттер названия пары
+    public void setLabel(String label) {
+        this.label = label;
+    }
+    // Геттер названия пары
+    public String getLabel() {
+        return label;
+    }
+    // Вывод информации о типах пары
+    @Override
+    public String toString() {
+        return String.format("%s: {%s, %s}",
+                label,
+                first.getClass().getSimpleName(),
+                second.getClass().getSimpleName());
+    }
+}
