0ee48459913e52406a91206f2a1f85d0a77870be
duahifnv <max135603@gmail.com>
2024-05-04 23:59:27 +0300

    Рефакторинг

--- lab7/src/lab7/task2/Task2.java
+++ lab7/src/lab7/task2/Task2.java
@@ -1,25 +1,70 @@
 package lab7.task2;
 
 import lab7.SceneUtils;
+
+import javax.imageio.ImageIO;
 import javax.swing.*;
+import javax.swing.ImageIcon;
+import javax.swing.border.EmptyBorder;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ComponentListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
 public class Task2 extends JFrame {
+    String imgPath = "D:\\DSTU\\ява\\lab7\\img\\neozid.jpg";
     public Task2() {
         JPanel container = new JPanel();
-        container.setLayout(new FlowLayout(FlowLayout.CENTER));
-        JLabel label = new JLabel();
-        label.setText("Image101");
-        label.setSize(container.getPreferredSize());
-        container.add(label);
+        container.setLayout(new BorderLayout(20, 15));
+        container.setBorder(new EmptyBorder(10, 10, 10, 10));
+        container.setBackground(Color.WHITE);
 
+        JLabel mylabel = new JLabel("<html>" + "Image101" + "</html>");
+        mylabel.setHorizontalAlignment(SwingConstants.CENTER);
+        mylabel.setFont(new Font(Font.SANS_SERIF, Font.PLAIN, 26));
+        container.add(mylabel, BorderLayout.NORTH);
+
+        JPanel imageContainer = new JPanel();
+        imageContainer.setPreferredSize(new Dimension(1000, 600));
+        FilterImage filterImage = new FilterImage(readImage(imgPath));
+        filterImage.setPreferredSize(imageContainer.getPreferredSize());
+        imageContainer.add(filterImage);
+        container.add(imageContainer, BorderLayout.WEST);
+
+        JPanel btnContainer = new JPanel();
+        btnContainer.setLayout(new BoxLayout(btnContainer, BoxLayout.Y_AXIS));
+
+        Button rotateBtn = new Button("Повернуть на 180 градусов");
+        rotateBtn.addActionListener(e -> filterImage.setFilter(Filter.ROTATE));
+
+        Button grayScaleBtn = new Button("Черно-белый фильтр");
+        grayScaleBtn.addActionListener(e -> filterImage.setFilter(Filter.GRAYSCALE));
+
+        Button blurBtn = new Button("Заблюрить");
+        blurBtn.addActionListener(e -> filterImage.setFilter(Filter.BLUR));
+
+        btnContainer.add(rotateBtn);
+        btnContainer.add(grayScaleBtn);
+        btnContainer.add(blurBtn);
+        container.add(btnContainer, BorderLayout.EAST);
         //SceneUtils.configFrame(jPanel);
         add(container);
-        setSize(800, 500);
+        setSize(1280, 720);
+        setResizable(false);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
     }
-
+    private static BufferedImage readImage(String imgPath) {
+        try {
+           return ImageIO.read(new File(imgPath));
+        }
+        catch (IOException e) {
+            throw new Error("Не удалось обработать изображение");
+        }
+    }
     public static void main(String[] args) {
         SwingUtilities.invokeLater(Task2::new);
     }
