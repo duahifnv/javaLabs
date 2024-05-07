fb787ce9dd936ef9e115e3349548599c3ee4888a
duahifnv <max135603@gmail.com>
2024-05-07 14:32:34 +0300

    Обновлен дизайн и добавлена возможность ставить картинки вместо текста

--- lab7/src/lab7/elements/Button.java
+++ lab7/src/lab7/elements/Button.java
@@ -1,15 +1,31 @@
 package lab7.elements;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.util.Objects;
 
 public class Button extends JButton {
-    public Button(String label) {
-        setText(label);
-        setFont(new Font(Font.SANS_SERIF, Font.PLAIN, 16));
-        setPreferredSize(new Dimension(200, 50));
-        setBorder(new RoundedBorder(10));
+    public Button() {
+        setFont(new Font(Font.SANS_SERIF, Font.BOLD, 16));
+        setForeground(Color.BLACK);
         setBackground(Color.WHITE);
+        setBorder(new RoundedBorder(10));
+        setBorderPainted(false);
+        setFocusPainted(false);
+        setContentAreaFilled(false);
+        setPreferredSize(new Dimension(200, 50));
+    }
+    public void setIcon(String path) {
+        try {
+            Image img = ImageIO.read(new File(path));
+            setIcon(new ImageIcon(img));
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
 
