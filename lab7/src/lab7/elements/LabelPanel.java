c5ceaffb2c261840b006fc799dab9c2349e22603
duahifnv <max135603@gmail.com>
2024-05-09 02:13:01 +0300

    Добавлен динамический размер шрифта

--- lab7/src/lab7/elements/LabelPanel.java
+++ lab7/src/lab7/elements/LabelPanel.java
@@ -1,13 +1,17 @@
 package lab7.elements;
 
 import javax.swing.*;
+import java.awt.*;
 
 public abstract class LabelPanel extends JPanel {
     protected JLabel label = new JLabel();
     public LabelPanel() {
+        label.setFont(label.getFont().deriveFont((float)(label.getFont().getSize() - 5)));
+        label.setAlignmentX(Component.RIGHT_ALIGNMENT);
+        setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
         add(label);
     }
-    public void setLabel(String label) {
-        this.label.setText(label);
+    public void setLabel(String text) {
+        label.setText(text);
     }
 }
