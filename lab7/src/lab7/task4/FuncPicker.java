2cb3c7b73481a8aa1f58ce45d71d5ea5a2006c96
duahifnv <max135603@gmail.com>
2024-05-09 00:50:15 +0300

    Рефакторинг

--- lab7/src/lab7/task4/FuncPicker.java
+++ lab7/src/lab7/task4/FuncPicker.java
@@ -1,26 +1,35 @@
 package lab7.task4;
 
+import lab7.elements.LabelPanel;
 import lab7.task4.graph.Func;
 import lab7.task4.graph.SingleGraph;
 
 import javax.swing.*;
+import java.awt.*;
 
-public class FuncPicker extends JComboBox<Func> {
+public class FuncPicker extends LabelPanel {
+    private JComboBox<Func> comboBox;
     private Func func;
     private SingleGraph graph;
     public FuncPicker(Func func, SingleGraph graph) {
+        label = new JLabel("Функция");
+        label.setFont(new Font("MONTSERRAT", Font.PLAIN, 20));
+        add(this.label);
+
+        comboBox = new JComboBox<>();
         this.func = func;
         this.graph = graph;
         for (Func f : Func.values()) {
-            addItem(f);
+            comboBox.addItem(f);
         }
-        addActionListener(event -> {
-            int index = getSelectedIndex();
-            Func f = getItemAt(index);
+        comboBox.addActionListener(event -> {
+            int index = comboBox.getSelectedIndex();
+            Func f = comboBox.getItemAt(index);
             if (f == this.func) return;
             this.func = f;
             graph.setCurve(f);
             graph.repaint();
         });
+        add(comboBox);
     }
 }
