1d3e2f530cd466114669698a5da07fb21ef911ed
duahifnv <max135603@gmail.com>
2024-05-09 00:46:04 +0300

    Менеджер тем

---
+++ lab7/src/lab7/elements/ThemeManager.java
@@ -0,0 +1,15 @@
+package lab7.elements;
+
+import lab7.theme.Theme;
+
+import javax.swing.*;
+
+public class ThemeManager {
+    public static void initTheme(Theme theme) {
+        UIManager.put("Panel.background", theme.getBackground());
+        UIManager.put("Label.font", theme.getFont().deriveFont((float)(theme.getFont().getSize() + 10)));
+        UIManager.put("Label.foreground", theme.getForeground());
+        UIManager.put("Panel.foreground", theme.getForeground());
+        UIManager.put("Panel.font", theme.getFont());
+    }
+}
