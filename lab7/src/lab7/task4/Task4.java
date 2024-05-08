bbd8f5a387e98d5362c7730709084cb058b55ee0
duahifnv <max135603@gmail.com>
2024-05-09 00:49:44 +0300

    Рефакторинг

--- lab7/src/lab7/task4/Task4.java
+++ lab7/src/lab7/task4/Task4.java
@@ -2,27 +2,34 @@ package lab7.task4;
 
 import lab7.elements.Container;
 import lab7.elements.Label;
+import lab7.elements.ThemeManager;
 import lab7.task4.graph.*;
+import lab7.theme.Theme;
 
 import javax.swing.*;
 import java.awt.*;
 
 public class Task4 extends JFrame {
     public Task4() {
+        ThemeManager.initTheme(Theme.LIGHT);
         JPanel container = new Container();
         container.add(new Label("Graph"), BorderLayout.NORTH);
 
         SingleGraph graph = new SingleGraph(Func.SIN);
         graph.setPreferredSize(new Dimension(400, 400));
-        graph.setBackground(Color.WHITE);
         container.add(graph);
+
         JPanel navContainer = new JPanel();
-        navContainer.setLayout(new FlowLayout());
+        navContainer.setLayout(new BoxLayout(navContainer, BoxLayout.Y_AXIS));
         navContainer.setPreferredSize(new Dimension(200, 400));
 
         FuncPicker funcPicker = new FuncPicker(Func.SIN, graph);
-
         navContainer.add(funcPicker);
+
+        ColorPicker colorPicker = new ColorPicker();
+        colorPicker.setLabel("Цвет кривой");
+        navContainer.add(colorPicker);
+
         container.add(navContainer, BorderLayout.EAST);
         add(container);
         setSize(1280, 720);
