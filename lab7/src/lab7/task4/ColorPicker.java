206905c3131d6e247d2a22ff5a50cdde407b93b2
duahifnv <max135603@gmail.com>
2024-05-09 02:10:54 +0300

    Удалены лишние вкладки с цветовыми схемами

--- lab7/src/lab7/task4/ColorPicker.java
+++ lab7/src/lab7/task4/ColorPicker.java
@@ -3,12 +3,27 @@ package lab7.task4;
 import lab7.elements.LabelPanel;
 
 import javax.swing.*;
+import javax.swing.colorchooser.AbstractColorChooserPanel;
+import javax.swing.colorchooser.ColorSelectionModel;
+import javax.swing.event.ChangeListener;
+import java.awt.*;
 
 public class ColorPicker extends LabelPanel {
     private JColorChooser colorChooser;
     public ColorPicker() {
         label.setText("Выбор цвета");
         colorChooser = new JColorChooser();
+        AbstractColorChooserPanel[] panels = colorChooser.getChooserPanels();
+        for (AbstractColorChooserPanel accp : panels) {
+            if(!accp.getDisplayName().equals("RGB")) {
+                colorChooser.removeChooserPanel(accp);
+            }
+        }
+
+        colorChooser.setPreviewPanel(new JPanel());
         add(colorChooser);
+    }
+    public JColorChooser getColorChooser() {
+        return colorChooser;
     }
 }
