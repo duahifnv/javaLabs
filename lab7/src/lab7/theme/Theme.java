d5ccd6e514bf5f992fe1418cb0c6c42ed70d4b77
duahifnv <max135603@gmail.com>
2024-05-09 00:45:51 +0300

    Перечисляемый тип тем

---
+++ lab7/src/lab7/theme/Theme.java
@@ -0,0 +1,25 @@
+package lab7.theme;
+
+import java.awt.*;
+
+public enum Theme {
+    LIGHT(Color.BLACK, Color.WHITE),
+    COFFEE(new Color(0x564C41), new Color(0x1a130f)),
+    FOREST(new Color(0x2f3811), new Color(0x555c33));
+    private Color foreground;
+    private Color background;
+    private Font font = new Font("MONTSERRAT", Font.PLAIN, 16);
+    Theme(Color foreground, Color background) {
+        this.foreground = foreground;
+        this.background = background;
+    }
+    public Color getForeground() {
+        return foreground;
+    }
+    public Color getBackground() {
+        return background;
+    }
+    public Font getFont() {
+        return font;
+    }
+}
