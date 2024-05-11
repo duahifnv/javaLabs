fd7ba698fe4f5a5352c4559995f3c1947ffae5ad
duahifnv <max135603@gmail.com>
2024-05-11 22:06:13 +0300

    Логика

--- lab7/src/lab7/task8/CellCore.java
+++ lab7/src/lab7/task8/CellCore.java
@@ -1,14 +1,30 @@
 package lab7.task8;
 
 import lab7.elements.LabelPanel;
+import lab7.task6.Dice;
 
+import javax.swing.*;
 import java.awt.*;
 
 public class CellCore extends LabelPanel {
+    private int size;
     public CellCore() {
-        setPreferredSize(new Dimension(DiceGame.frameSize / 3, DiceGame.frameSize / 3));
-        setLayout(new FlowLayout());
+        size = DiceGame.frameSize / 3;
+        setPreferredSize(new Dimension(size, size));
+        setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
         setLabel("Dice Game");
         offsetFontSize(10);
+        JPanel diceWrapper = new JPanel();
+        diceWrapper.setLayout(new FlowLayout());
+        diceWrapper.setMinimumSize(new Dimension(size / 5, size / 5));
+        Dice dice = new Dice(size / 5);
+        dice.setBorderRadius(20);
+        diceWrapper.add(dice);
+        add(diceWrapper);
+        ScoreField first = new ScoreField("Player 1");
+        first.setScore(20);
+        ScoreField second = new ScoreField("Player 2");
+        add(first);
+        add(second);
     }
 }
