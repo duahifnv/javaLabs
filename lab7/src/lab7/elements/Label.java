260ef2d21149ab23b6fd57f65a2de952e9a3427a
duahifnv <max135603@gmail.com>
2024-05-11 22:05:26 +0300

    Метод оффсета размера шрифта

--- lab7/src/lab7/elements/Label.java
+++ lab7/src/lab7/elements/Label.java
@@ -4,10 +4,13 @@ import javax.swing.*;
 
 public class Label extends JLabel {
     public Label(String text) {
-        setText("<html>" + text + "</html>");
+        super.setText("<html>" + text + "</html>");
         setHorizontalAlignment(SwingConstants.CENTER);
     }
     public void setText(String text) {
         super.setText("<html>" + text + "</html>");
+    }
+    public void offsetFontSize(int size) {
+        setFont(getFont().deriveFont((float)(getFont().getSize() + size)));
     }
 }
