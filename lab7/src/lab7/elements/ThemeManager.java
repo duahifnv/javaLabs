73b3bfe46699d1cc42d88fe05525e700c954d515
duahifnv <max135603@gmail.com>
2024-05-11 11:22:43 +0300

    Добавлен импорт

--- lab7/src/lab7/elements/ThemeManager.java
+++ lab7/src/lab7/elements/ThemeManager.java
@@ -7,9 +7,9 @@ import javax.swing.*;
 public class ThemeManager {
     public static void initTheme(Theme theme) {
         UIManager.put("Panel.background", theme.getBackground());
+        UIManager.put("Panel.foreground", theme.getForeground());
         UIManager.put("Label.font", theme.getFont().deriveFont((float)(theme.getFont().getSize() + 10)));
         UIManager.put("Label.foreground", theme.getForeground());
-        UIManager.put("Panel.foreground", theme.getForeground());
         UIManager.put("Panel.font", theme.getFont());
         UIManager.put("Button.background", theme.getBackground());
         UIManager.put("Button.foreground", theme.getForeground());
