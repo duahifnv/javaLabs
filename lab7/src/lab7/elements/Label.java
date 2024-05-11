cfe24b4a067019a576a4bcbf6f079bd5bfe62dda
duahifnv <max135603@gmail.com>
2024-05-11 21:25:23 +0300

    Метод изменения текста

--- lab7/src/lab7/elements/Label.java
+++ lab7/src/lab7/elements/Label.java
@@ -1,11 +1,13 @@
 package lab7.elements;
 
 import javax.swing.*;
-import java.awt.*;
 
 public class Label extends JLabel {
     public Label(String text) {
         setText("<html>" + text + "</html>");
         setHorizontalAlignment(SwingConstants.CENTER);
+    }
+    public void setText(String text) {
+        super.setText("<html>" + text + "</html>");
     }
 }
