ffeda0b8cf6882c430f5ec04d5459f389b5d9b51
duahifnv <max135603@gmail.com>
2024-05-12 16:14:42 +0300

    Панель с полям интервала графика функции

---
+++ lab7/src/lab7/task4/IntervalField.java
@@ -0,0 +1,40 @@
+package lab7.task4;
+
+import lab7.elements.Button;
+import lab7.elements.Label;
+import lab7.elements.LabelPanel;
+import lab7.task3.TextField;
+import lab7.task3.TextPrompt;
+
+import javax.swing.*;
+
+public class IntervalField extends LabelPanel {
+    public IntervalField() {
+        setLabel("Интервал");
+        JTextField fromField = new TextField();
+        TextPrompt fromPrompt = new TextPrompt("От", fromField, TextPrompt.Show.FOCUS_LOST);
+        add(fromField);
+
+        JTextField toField = new TextField();
+        TextPrompt toPrompt = new TextPrompt("До", toField, TextPrompt.Show.FOCUS_LOST);
+        add(toField);
+
+        Label errorMsg = new Label("Неверный ввод");
+        errorMsg.setVisible(false);
+        errorMsg.offsetFontSize(-8);
+        add(errorMsg);
+        Button submitBtn = new Button();
+        submitBtn.setText("Установить");
+        submitBtn.addActionListener(e -> {
+            try {
+                double from = Double.parseDouble(fromField.getText());
+                double to = Double.parseDouble(toField.getText());
+                errorMsg.setVisible(false);
+            }
+            catch (NumberFormatException | NullPointerException ne) {
+                errorMsg.setVisible(true);
+            }
+        });
+        add(submitBtn);
+    }
+}
