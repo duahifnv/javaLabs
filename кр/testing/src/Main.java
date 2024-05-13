c9610a947dbab4af627e1fdc86712b041d99f744
duahifnv <max135603@gmail.com>
2024-05-14 00:10:28 +0300

    update

--- кр/testing/src/Main.java
+++ кр/testing/src/Main.java
@@ -4,24 +4,16 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        ArrayList< Integer > li = new ArrayList< Integer >();
+        ArrayList< Integer > li = new ArrayList<>();
         li.add(1);
         ArrayList< ? > io = li;
         io.add(new Object());
         System.out.println(li.size());
     }
 }
-interface IA {
-    void f();
-}
-interface IB {
-    void f();
-}
-class A {
-    private int a;
-    public A(int x) {
-        a = x;
-        System.out.println("Hello");
-    }
-}
-
+interface I1 {}
+interface I2 {}
+class A {}
+class B<T extends A implements I1, I2> {}
+class B1<T extends A & I1 & I2> {}
+class B2<T extends A, I1, I2> {}
